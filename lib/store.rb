6a012608531e1ea0785403f00edf84e62107db64
John O'Halloran <john.ohalloran@telus.com>
2023-01-23 20:54:05 -0500

    new validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,6 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  validates :name, presence: true
+  validates :name, :annual_revenue, :mens_apparel, :womens_apparel, presence: true, 
   validates :name, length: {minimum: 3}
-  validates :annual_revenue, numericality: { only_integer: true }
-  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
